12d98a7626a645f959f160a6f48ed0008ccbdac6
Otavio Mello <otaviodemello15@gmail.com>
2022-08-11 15:07:02 -0300

    Created Customer microservice base files and configured the h2 database

---
+++ mscustomer/src/test/java/br/com/project/shopstyle/mscustomer/MscustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.shopstyle.mscustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
