4cea19a18b02b5a94fc5e14ac6c94c567f8a470c
Otavio Mello <otaviodemello15@gmail.com>
2022-08-16 15:30:13 -0300

    Created Catalog Microservice base files

---
+++ mscatalog/src/main/java/br/com/project/shopstyle/mscatalog/MscatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.shopstyle.mscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscatalogApplication.class, args);
+	}
+
+}
